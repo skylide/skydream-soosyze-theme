3b22455391f9a99c715b34ca319ebae0ea18d3c8
skylide <dupont.arnaud.58@gmail.com>
2020-05-01 23:05:52 +0200

    Update html.php

--- html.php
+++ html.php
@@ -19,6 +19,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.16.0/css/mdb.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.3.1/css/hover-min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=PT+Sans+Caption|Permanent+Marker" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo $base_theme; ?>assets/css/styles.css">
     <?php echo $styles; ?>
